c517e71cc9c93a288d0458e559a2104e33ba4590
Ehsan Mashali <e.mashali@roundtableapps.com>
2023-05-13 10:22:53 +0330

    add env in project

--- pages/menu/[id].js
+++ pages/menu/[id].js
@@ -14,7 +14,7 @@ const Details = ({ data }) => {
 export default Details;
 
 export async function getStaticPaths() {
-  const res = await fetch("http://localhost:4000/data");
+  const res = await fetch(`${process.env.BASE_URL}/data`);
   const json = await res.json();
   const data = json.slice(0, 10);
 
@@ -30,7 +30,7 @@ export async function getStaticProps(context) {
   const {
     params: { id },
   } = context;
-  const res = await fetch(`http://localhost:4000/data/${id}`);
+  const res = await fetch(`${process.env.BASE_URL}/data/${id}`);
   const data = await res.json();
 
   if (!data.id) {
@@ -41,6 +41,6 @@ export async function getStaticProps(context) {
 
   return {
     props: { data },
-    revalidate: 60 * 60 * 10,
+    revalidate: +process.env.REVALIDATE,
   };
 }
